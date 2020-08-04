8c1b4795bbaf006d97a155714532a253ee3ca75c
Topher <mcconkey@gmail.com>
2020-08-04 06:40:06 -0500

    database.js config

--- config/database.js
+++ config/database.js
@@ -17,3 +17,19 @@ module.exports = ({ env }) => ({
               },
       },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
